9bd57c582ce44b9a61553d478d3bcf16ef26b430
Kacey <ogrippey@gmail.com>
2021-02-24 16:29:19 +0000

    gpio.c comms

--- src/gpio.c
+++ src/gpio.c
@@ -1,3 +1,5 @@
+#include "gpio.h"
+
 unsigned int *gpset1 = 0xFE200020;
 unsigned int *gpsel4 = 0xFE200010;
 unsigned int *gpclr1 = 0xFE20002C;
@@ -8,15 +10,15 @@ void led_init(){
 	*gpsel4 = *gpsel4 | (1 << 6);
 	return;
 }
-
+//turn led on by writing a 1 to gpio pen 
 void led_on(){
 	*gpset1 = (1 << 10);
 }
-
+//write to gpclr to turn led off
 void led_off(){
 	*gpclr1 = (1 << 10);
 }
-
+//simple delay function
 void delay(){
 	unsigned int i = 0;
 	for(i; i < 10000; i++){
